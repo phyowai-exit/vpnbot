e0efda4cc712e53faae3b28fc962e3a5f7ab4520
phyowai-exit <phyo.w28@gmail.com>
2024-10-11 05:59:59 +0630

    Update inline.py

--- tgbot/keyboards/inline.py
+++ tgbot/keyboards/inline.py
@@ -7,19 +7,19 @@ logger = logging.getLogger(__name__)
 
 def keyboard_start():
     builder = InlineKeyboardBuilder()
-    builder.button(text='Доступ к VPN', callback_data='vpn')
-    builder.button(text='Что за VPN?', callback_data='help')
+    builder.button(text='VPN KEY ယူမယ်', callback_data='vpn')
+    builder.button(text='ဘယ်လို VPN အမျိုးအစားလဲ။', callback_data='help')
     builder.adjust(2)
     return builder.as_markup()
 
 
 def keyboard_help():
     builder = InlineKeyboardBuilder()
-    builder.button(text='Клиенты для подключения', url='https://docs.marzban.dev/start/reality_app/')
+    builder.button(text='ဆော့ဝဲ Donwload လုပ်ရန်', url='https://play.google.com/store/apps/details?id=dev.hexasoftware.v2box&pcampaignid=web_share')
     return builder.as_markup()
 
 
 def keyboard_cancel():
     builder = InlineKeyboardBuilder()
-    builder.button(text='❌Выйти из меню', callback_data='cancel')
+    builder.button(text='❌ ထွက်မည်', callback_data='cancel')
     return builder.as_markup()
