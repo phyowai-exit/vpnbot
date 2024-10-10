4b1730a543fc93ae791363a5eec419b7d50afff6
phyowai-exit <phyo.w28@gmail.com>
2024-10-11 06:01:17 +0630

    Update cancel.py

--- tgbot/handlers/cancel.py
+++ tgbot/handlers/cancel.py
@@ -15,12 +15,12 @@ async def cancel_handler(message: Message, state: FSMContext):
         return
 
     await state.clear()
-    await message.answer('Действие отменено.')
+    await message.answer('လုပ်ဆောင်ချက်ကို ပယ်ဖျက်လိုက်ပါပြီ။')
 
 
 @cancel_router.callback_query(F.data == 'cancel')
 async def cancel_callback(callback_query: CallbackQuery, state: FSMContext):
-    await callback_query.answer('Действие отменено.')
+    await callback_query.answer('လုပ်ဆောင်ချက်ကို ပယ်ဖျက်လိုက်ပါပြီ။')
     await bot.delete_message(chat_id=callback_query.message.chat.id, message_id=callback_query.message.message_id)
     current_state = await state.get_state()
     if current_state is None:
